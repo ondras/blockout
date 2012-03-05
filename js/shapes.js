c6e726bdf3f9863ac38d8fff66692c27d38b9b0a
ondras <none@none>
2012-03-05 21:45:12 +0100

    polishing

--- js/shapes.js
+++ js/shapes.js
@@ -1,21 +1,14 @@
-Shape._flat = ["1x1", "2x2", "2x1", "3x1", "4x1", "Corner", "Submarine", "Z"];
-Shape._advanced = Shape._flat.concat();
+Shape._flat = ["2x2", "2x1", "3x1", "4x1", "Corner", "Submarine", "Z"];
+Shape._advanced = Shape._flat.concat("Corner3d", "Corner3d", "Snake1", "Snake1", "Snake2", "Snake2");
 
 Shape.newFlat = function(pit) {
 	var name = "new" + this._flat.random();
-	name = "newCorner3d";
 	return this[name](pit);
 }
 
 Shape.newAdvanced = function(pit) {
 	var name = "new" + this._advanced.random();
 	return this[name](pit);
-}
-
-Shape.new1x1 = function(pit) {
-	var shape = new this(pit);
-	shape.addCube([0, 0, 0]);
-	return shape;
 }
 
 Shape.new2x2 = function(pit) {
@@ -59,6 +52,15 @@ Shape.newCorner = function(pit) {
 	return shape;
 }
 
+Shape.newL = function(pit) {
+	var shape = new this(pit);
+	shape.addCube([0, 0, 0]).hideFaces([Face.RIGHT]);
+	shape.addCube([1, 0, 0]).hideFaces([Face.LEFT, Face.RIGHT]);
+	shape.addCube([2, 0, 0]).hideFaces([Face.LEFT, Face.BOTTOM]);
+	shape.addCube([2, 1, 0]).hideFaces([Face.TOP]);
+	return shape;
+}
+
 Shape.newSubmarine = function(pit) {
 	var shape = new this(pit);
 	shape.addCube([0, 0, 0]).hideFaces([Face.RIGHT]);
@@ -83,5 +85,23 @@ Shape.newCorner3d = function(pit) {
 	shape.addCube([0, 0, 1]).hideFaces([Face.FRONT, Face.RIGHT, Face.BOTTOM]);
 	shape.addCube([1, 0, 1]).hideFaces([Face.LEFT]);
 	shape.addCube([0, 1, 1]).hideFaces([Face.TOP]);
+	return shape;
+}
+
+Shape.newSnake1 = function(pit) {
+	var shape = new this(pit);
+	shape.addCube([0, 0, 0]).hideFaces([Face.RIGHT]);
+	shape.addCube([1, 0, 0]).hideFaces([Face.LEFT, Face.BOTTOM]);
+	shape.addCube([1, 1, 0]).hideFaces([Face.TOP, Face.BACK]);
+	shape.addCube([1, 1, 1]).hideFaces([Face.FRONT]);
+	return shape;
+}
+
+Shape.newSnake2 = function(pit) {
+	var shape = new this(pit);
+	shape.addCube([0, 1, 0]).hideFaces([Face.RIGHT]);
+	shape.addCube([1, 1, 0]).hideFaces([Face.LEFT, Face.TOP]);
+	shape.addCube([1, 0, 0]).hideFaces([Face.BOTTOM, Face.BACK]);
+	shape.addCube([1, 0, 1]).hideFaces([Face.FRONT]);
 	return shape;
 }
